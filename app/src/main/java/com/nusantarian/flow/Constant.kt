242a07227563612f26f212a6ef69f6b37d3973c3
rogcomfox <ygladiensyah@gmail.com>
2020-02-19 00:18:13 +0700

    add constant class

---
+++ app/src/main/java/com/nusantarian/flow/Constant.kt
@@ -0,0 +1,11 @@
+package com.nusantarian.flow
+
+class Constant {
+
+    companion object {
+        const val TAG: String = "Flow"
+        const val ERROR_DIALOG_REQUEST:Int = 9001
+        const val PERMISSIONS_REQUEST_ENABLE_GPS:Int = 9003
+        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:Int = 9002
+    }
+}
