b133c6ff015fe018321629d02561c7272d07d01f
rogcomfox <ygladiensyah@gmail.com>
2020-03-01 15:25:40 +0700

    add adapter

---
+++ app/src/main/java/com/nusantarian/flow/adapter/TrashAdapter.kt
@@ -0,0 +1,20 @@
+package com.nusantarian.flow.adapter
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TrashAdapter(val items : ArrayList<String>, val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
--- app/src/main/java/com/nusantarian/flow/fragment/TrashListFragment.kt
+++ app/src/main/java/com/nusantarian/flow/fragment/TrashListFragment.kt
@@ -1,25 +1,39 @@
 package com.nusantarian.flow.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
-
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.nusantarian.flow.R
 
 class TrashListFragment : Fragment() {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<ViewDataBinding>(inflater, R.layout.fragment_trash_list, container, false)
-        val view = binding.root
+        val view = inflater.inflate(R.layout.fragment_trash_list, container, false)
+        toolbar = view.findViewById(R.id.toolbar)
+        progressBar = view.findViewById(R.id.progress_circular)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).supportActionBar?.title = "List Sampah"
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        progressBar.visibility = View.VISIBLE
+        initRecycler()
         return view
+    }
+
+    private fun initRecycler() {
+
     }
 
 }
