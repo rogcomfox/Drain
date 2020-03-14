cd347d511fcdcc5dc039671fc8401aada2fdd6c6
rogcomfox <ygladiensyah@gmail.com>
2020-03-15 06:00:05 +0700

    add trash list fragment

--- app/src/main/java/com/nusantarian/flow/Constant.kt
+++ app/src/main/java/com/nusantarian/flow/Constant.kt
@@ -4,8 +4,9 @@ class Constant {
 
     companion object {
         const val TAG: String = "Flow"
-        const val ERROR_DIALOG_REQUEST:Int = 9001
-        const val PERMISSIONS_REQUEST_ENABLE_GPS:Int = 9003
-        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:Int = 9002
+        const val ERROR_DIALOG_REQUEST: Int = 9001
+        const val PERMISSIONS_REQUEST_ENABLE_GPS: Int = 9003
+        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: Int = 9002
+        const val MAPVIEW_BUNDLE_KEY: String = "MapViewBundleKey"
     }
 }
--- app/src/main/java/com/nusantarian/flow/activity/SplashActivity.kt
+++ app/src/main/java/com/nusantarian/flow/activity/SplashActivity.kt
@@ -1,9 +1,9 @@
 package com.nusantarian.flow.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.nusantarian.flow.R
 
 class SplashActivity : AppCompatActivity() {
